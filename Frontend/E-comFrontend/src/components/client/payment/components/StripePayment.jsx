1afe0a063709e74e3135234122a06eb27e961a9d
Fahim550 <122682768+Fahim550@users.noreply.github.com>
2024-06-08 23:16:23 +0600

    server link change

--- Frontend/E-comFrontend/src/components/client/payment/components/StripePayment.jsx
+++ Frontend/E-comFrontend/src/components/client/payment/components/StripePayment.jsx
@@ -75,7 +75,7 @@ const CheckoutForm = ({ userInfo }) => {
     console.log("OrderProducts", OrderProducts);
 
     await postData(
-      "https://mern-e-com-xi.vercel.app/orders",
+      "https://mern-e-com-one.vercel.app/orders",
       OrderProducts
     ).then((response) => {
       if (response.code == 200) {
